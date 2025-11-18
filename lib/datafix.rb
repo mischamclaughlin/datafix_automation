b70d54480fedd84a4bc7910d0a25148b8d28820a
Mischa McLaughlin <mischa.mclaughlin@sage.com>
2025-11-18 11:57:12 +0000

    Add link to config file

--- lib/datafix.rb
+++ lib/datafix.rb
@@ -1,4 +1,18 @@
+require 'yaml'
+
 module DataFix
+  CONFIG_PATHS = [
+    File.expand_path('../../config/datafix.yml', __dir__),
+    File.expand_path('../config/datafix.yml', __dir__),
+    File.expand_path('config/datafix.yml', Dir.pwd)
+  ].freeze
+
+  def self.config
+    @config ||= begin
+      path = CONFIG_PATHS.find { |p| File.exist?(p) }
+      path ? (YAML.load_file(path) || {}) : {}
+    end
+  end
 end
 
 require 'datafix/parse_files'
