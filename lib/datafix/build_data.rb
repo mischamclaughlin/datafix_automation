4bb968cf9fedb86bf8ac11d96bb78502adfa8225
Mischa McLaughlin <mischa.mclaughlin@sage.com>
2025-11-20 11:44:52 +0000

    Change logic for building file & fix tests broken due to logic change

--- lib/datafix/build_data.rb
+++ lib/datafix/build_data.rb
@@ -13,50 +13,103 @@ module DataFix
         end
     end
 
+    def build_account_data
+      build_all_data('accounts')
+    end
+
+    def build_subscription_data
+      build_all_data('subscriptions')
+    end
+
     def build_all_data(options = 'all')
+      include_accounts = options == 'all' || options == 'accounts'
+      include_subscriptions = options == 'all' || options == 'subscriptions'
+
       built_data = []
-      seen_main_values = []
+      seen_accounts = {}
+      subscription_counts = Hash.new(0)
 
       @parsed_input_data.each do |entry|
         entry.each do |main_value, nested_data|
-          @parsed_php_admin_data.each do |php_entry|
-            next unless php_entry.key?(main_value)
-            if php_entry[main_value]['zuora_subscription_number']
-              zuora_number = php_entry[main_value]['zuora_subscription_number'].strip
-            elsif seen_main_values.include?(main_value)
-              zuora_number = nested_data['subscription_number_created_2'].strip
-            else
-              zuora_number = nested_data['subscription_number_created_1'].strip
-              seen_main_values << main_value
+          php_data = php_data_for(main_value)
+          next unless php_data
+
+          nested_data ||= {}
+          client_guid = main_value.to_s
+
+          data = { client_business_guid: client_guid }
+
+          if include_accounts
+            if options == 'accounts' && seen_accounts[client_guid]
+              next
             end
 
-            data = {
-              'client_business_guid': main_value,
-            }
+            client_business_id = fetch_value(php_data, 'client_business_id')
+            data[:client_business_id] = client_business_id.to_i if client_business_id
 
-            if options == 'all' || options == 'accounts'
-              next if built_data.any? { |d| d[:client_business_guid] == main_value } && options == 'accounts'
-              data['client_business_id'] = php_entry[main_value]['client_business_id'].to_i
-              data['zuora_account_number'] = nested_data['zuora_account_number_for_client'].strip
-              if @old_data
-                data['old_zuora_account_number'] = php_entry[main_value]['zuora_account_number']&.strip || nil
-              end
+            account_number = clean_string(fetch_value(nested_data, 'zuora_account_number_for_client'))
+            data[:zuora_account_number] = account_number if account_number
+
+            if @old_data
+              old_account = clean_string(fetch_value(php_data, 'zuora_account_number'))
+              data[:old_zuora_account_number] = old_account if old_account
             end
 
-            if options == 'all' || options == 'subscriptions'
-              data['sub_id'] = php_entry[main_value]['sub_id'].to_i
-              data['zuora_subscription_number'] = zuora_number
-              if @old_data
-                data['old_zuora_subscription_number'] = php_entry[main_value]['zuora_subscription_number']&.strip || nil
-              end
-            end
-
-            built_data << data
+            seen_accounts[client_guid] = true if options == 'accounts'
           end
+
+          if include_subscriptions
+            sub_id = fetch_value(php_data, 'sub_id')
+            data[:sub_id] = sub_id.to_i if sub_id
+
+            old_subscription = clean_string(fetch_value(php_data, 'zuora_subscription_number'))
+
+            new_subscription = nil
+            occurrence = subscription_counts[client_guid]
+            sub_key = occurrence.zero? ? 'subscription_number_created_1' : 'subscription_number_created_2'
+            new_subscription = clean_string(fetch_value(nested_data, sub_key))
+            subscription_counts[client_guid] += 1 if new_subscription
+
+            data[:zuora_subscription_number] = new_subscription if new_subscription
+            data[:old_zuora_subscription_number] = old_subscription if @old_data
+          end
+
+          built_data << data
         end
       end
 
       built_data
+    end
+
+    private
+
+    def php_data_for(main_value)
+      @parsed_php_admin_data.each do |php_entry|
+        php_data = fetch_value(php_entry, main_value)
+        return php_data unless php_data.nil?
+      end
+      nil
+    end
+
+    def fetch_value(hash, key)
+      return nil unless hash
+
+      return hash[key] if hash.key?(key)
+
+      string_key = key.to_s
+      return hash[string_key] if hash.key?(string_key)
+
+      symbol_key = key.respond_to?(:to_sym) ? key.to_sym : nil
+      return hash[symbol_key] if symbol_key && hash.key?(symbol_key)
+
+      nil
+    end
+
+    def clean_string(value)
+      return nil if value.nil?
+
+      stripped = value.to_s.strip
+      stripped.empty? ? nil : stripped
     end
   end
 end
--- spec/datafix_spec/build_data_spec.rb
+++ spec/datafix_spec/build_data_spec.rb
@@ -1,17 +1,19 @@
 require_relative '../spec_helper'
 
-RSpec.describe DataFix::ParseFiles do
-  describe '#build_account_data' do
+RSpec.describe DataFix::BuildData do
+  describe '#build_all_data' do
     let(:parsed_data) do
       [
         {
-          'guid-123' => {
-            'zuora_account_number_for_client' => ' ZUORA-001 '
-          }
-        },
-        {
-          'guid-456' => {
-            'zuora_account_number_for_client' => ' ZUORA-002 '
+          'business_guid_1': {
+            'zuora_account_number_for_client': 'A0001',
+            'subscription_number_created_1': 'S0001',
+            'subscription_number_created_2': 'S0002'
+          },
+          'business_guid_2': {
+            'zuora_account_number_for_client': 'A0002',
+            'subscription_number_created_1': 'S0003',
+            'subscription_number_created_2': 'S0004'
           }
         }
       ]
@@ -20,159 +22,60 @@ RSpec.describe DataFix::ParseFiles do
     let(:parsed_php_admin_data) do
       [
         {
-          'guid-123' => {
-            'client_business_id' => '1001'
-          }
-        },
-        {
-          'guid-456' => {
-            'client_business_id' => '1002'
+          'business_guid_1': {
+            'client_business_id': '101',
+            'zuora_account_number': 'OLD_A0001',
+            'sub_id': '201',
+            'zuora_subscription_number': 'OLD_S0001'
+          },
+          'business_guid_2': {
+            'client_business_id': '102',
+            'zuora_account_number': 'OLD_A0002',
+            'sub_id': '202',
+            'zuora_subscription_number': nil
           }
         }
       ]
     end
 
-    it 'builds account data correctly' do
-      builder = DataFix::BuildData.new(
+    let(:config) do
+      {
+        'settings': {
+          'old_data': true
+        }
+      }
+    end
+
+    subject do
+      DataFix::BuildData.new(
         parsed_data: parsed_data,
-        parsed_php_admin_data: parsed_php_admin_data
+        parsed_php_admin_data: parsed_php_admin_data,
+        config: config
       )
+    end
 
-      result = builder.build_account_data
-
+    it 'builds all data correctly' do
+      result = subject.build_all_data('all')
       expect(result).to eq([
         {
-          client_business_guid: 'guid-123',
-          client_business_id: 1001,
-          zuora_account_number: 'ZUORA-001'
+          client_business_guid: 'business_guid_1',
+          client_business_id: 101,
+          zuora_account_number: 'A0001',
+          old_zuora_account_number: 'OLD_A0001',
+          sub_id: 201,
+          zuora_subscription_number: 'S0001',
+          old_zuora_subscription_number: 'OLD_S0001'
         },
         {
-          client_business_guid: 'guid-456',
-          client_business_id: 1002,
-          zuora_account_number: 'ZUORA-002'
+          client_business_guid: 'business_guid_2',
+          client_business_id: 102,
+          zuora_account_number: 'A0002',
+          old_zuora_account_number: 'OLD_A0002',
+          sub_id: 202,
+          zuora_subscription_number: 'S0003',
+          old_zuora_subscription_number: nil
         }
       ])
-    end
-
-    it 'handles cases with no matching PHP admin data gracefully' do
-      parsed_php_admin_data_empty = []
-
-      builder = DataFix::BuildData.new(
-        parsed_data: parsed_data,
-        parsed_php_admin_data: parsed_php_admin_data_empty
-      )
-
-      result = builder.build_account_data
-
-      expect(result).to eq([])
-    end
-  end
-
-  describe '#build_subscription_data' do
-    let(:parsed_data) do
-      [
-        {
-          'guid-123' => {
-            'subscription_number_created_1' => ' SUB-001-A ',
-            'subscription_number_created_2' => ' SUB-001-B '
-          }
-        },
-        {
-          'guid-456' => {
-            'subscription_number_created_1' => ' SUB-002-A ',
-            'subscription_number_created_2' => ' SUB-002-B '
-          }
-        }
-      ]
-    end
-
-    let(:parsed_php_admin_data) do
-      [
-        {
-          'guid-123' => {
-            'sub_id' => '2001',
-            'zuora_subscription_number' => nil
-          }
-        },
-        {
-          'guid-456' => {
-            'sub_id' => '2002',
-            'zuora_subscription_number' => ' ZUORA-SUB-002 '
-          }
-        }
-      ]
-    end
-
-    it 'builds subscription data correctly' do
-      builder = DataFix::BuildData.new(
-        parsed_data: parsed_data,
-        parsed_php_admin_data: parsed_php_admin_data
-      )
-
-      result = builder.build_subscription_data
-
-      expect(result).to eq([
-        {
-          client_business_guid: 'guid-123',
-          sub_id: 2001,
-          zuora_subscription_number: 'SUB-001-A'
-        },
-        {
-          client_business_guid: 'guid-456',
-          sub_id: 2002,
-          zuora_subscription_number: 'ZUORA-SUB-002'
-        }
-      ])
-    end
-
-    it 'uses the second subscription number if the first has already been used' do
-      parsed_data_with_duplicate = [
-        {
-          'guid-123' => {
-            'subscription_number_created_1' => ' SUB-001-A ',
-            'subscription_number_created_2' => ' SUB-001-B '
-          }
-        },
-        {
-          'guid-123' => {
-            'subscription_number_created_1' => ' SUB-001-C ',
-            'subscription_number_created_2' => ' SUB-001-D '
-          }
-        }
-      ]
-
-      builder = DataFix::BuildData.new(
-        parsed_data: parsed_data_with_duplicate,
-        parsed_php_admin_data: parsed_php_admin_data
-      )
-
-      result = builder.build_subscription_data
-
-      expect(result).to eq([
-        {
-          client_business_guid: 'guid-123',
-          sub_id: 2001,
-          zuora_subscription_number: 'SUB-001-A'
-        },
-        {
-          client_business_guid: 'guid-123',
-          sub_id: 2001,
-          zuora_subscription_number: 'SUB-001-D'
-        }
-      ])
-    end
-
-    it 'handles cases with no subscriptions gracefully' do
-      empty_parsed_data = []
-
-      builder = DataFix::BuildData.new(
-        parsed_data: empty_parsed_data,
-        parsed_php_admin_data: parsed_php_admin_data
-      )
-
-      result = builder.build_subscription_data
-
-      expect(result).to eq([])
     end
   end
 end
